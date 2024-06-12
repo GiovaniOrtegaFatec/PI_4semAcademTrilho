8763d82a259df344489cd8c3fa6887b71a9fee12
Carlos Armindo Degasperi <111812436+CarlosDegasperi@users.noreply.github.com>
2024-06-12 15:06:34 -0300

    '12-06-2024'

--- PI_4sems/models.py
+++ PI_4sems/models.py
@@ -7,3 +7,16 @@ class Topic(models.Model):
     def __str__(self):
         return self.text
 # Create your models here.
+
+class Task(models.Model):
+    curso = models.CharField(max_length=100)
+    semestre = models.CharField(max_length=100)
+    diasemana = models.CharField(max_length=100)
+    horario = models.CharField(max_length=100)
+    materia = models.CharField(max_length=100)
+    professor = models.CharField(max_length=100)
+    sala = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.curso,self.semestre,self.diasemana,self.horario,self.materia,self.professor,self.sala
+    
--- PI_4sems/views.py
+++ PI_4sems/views.py
@@ -1,14 +1,66 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from datetime import datetime
+from PI_4sems.models import Task
+
+data_atual = datetime.now()
+dia_da_semana_numero = data_atual.weekday()
+dias_da_semana = ["Segunda-feira", "Terça-feira", "Quarta-feira", "Quinta-feira", "Sexta-feira", "Sábado", "Domingo"]
+dias_da_semana1= ["SEG","TER","QUA","QUI","SEX","SAB","DOM"]
+dia_da_semana = dias_da_semana[dia_da_semana_numero]
+dia_da_semana1= dias_da_semana1[dia_da_semana_numero]
+data_formatada = data_atual.strftime("%d/%m/%Y %H:%M:%S")
 
 def index(request):
-    """Página principal do PI_4sem"""
     return  render(request, 'PI_4sems/index.html')
 
 def telaprincipal(request):
-    return render(request, 'PI_4sems/TelaPrincipal.html')
+    if request.method == 'POST':
+        curso = request.POST.get('curso')
+        semestre = request.POST.get('semestre')
+        print('CURSO    POST:', curso)
+        print('SEMESTRE POST: ', semestre)
+        # Processamento...
+        print("Data:", data_formatada)
+        print("Dia da semana:", dia_da_semana)
+        print("Dia da semana:", dia_da_semana1)
+        tasks_curso = Task.objects.filter(curso=curso,semestre=semestre,diasemana=dia_da_semana1).first()
+        if tasks_curso:
+            return render(request, 'PI_4sems/TelaPrincipal.html', {'tasks_curso': tasks_curso, 'data': data_formatada, 'dia': dia_da_semana})
+        else:
+            return HttpResponse('Nenhuma tarefa encontrada para os critérios fornecidos.')
+        
+    else:
+        return HttpResponse('Método não permitido')
 
 def grade_curricular(request):
-    return render(request, 'PI_4sems/grade_curricular.html')
+    if request.method == 'POST':
+        curso = request.POST.get('curso')
+        semestre = request.POST.get('semestre')
+        print('CURSO    POST: ', curso)
+        print('SEMESTRE POST: ', semestre)
+        
+        # Processamento...
+        tasks = Task.objects.filter(curso=curso, semestre=semestre)
+        return render(request, 'PI_4sems/grade_curricular.html', {'tasks': tasks})
+    else:
+        return HttpResponse('Método não permitido')
+
 
 def mapa(request):
-    return render(request, 'PI_4sems/mapa.html')
+    if request.method == 'POST':
+        curso = request.POST.get('curso')
+        semestre = request.POST.get('semestre')
+        print('CURSO    POST: ', curso)
+        print('SEMESTRE POST: ', semestre)
+        
+        # Processamento...
+        tasks_mapa = Task.objects.filter(curso=curso, semestre=semestre,diasemana=dia_da_semana1).first()
+        print('SALA: ', tasks_mapa.sala+'.jpg')
+        sala_mapa = tasks_mapa.sala+'.jpg'
+        print('SALA: ', sala_mapa)
+        return render(request, 'PI_4sems/mapa.html', {'tasks_mapa': tasks_mapa, 'sala_mapa': sala_mapa})
+    else:
+        return HttpResponse('Método não permitido')
+
+
